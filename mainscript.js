ead442e850ddb0387dc84b3dbec98bc9f3f82cee
DiosTlaloc <diostlaloc5@gmail.com>
2026-01-02 15:14:18 -0800

    Implemented playRound function

--- mainscript.js
+++ mainscript.js
@@ -1,4 +1,6 @@
 
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -10,3 +12,43 @@ function getHumanChoice() {
     const humanChoice = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
     return humanChoice
 }
+
+function playRound(humanChoice, computerChoice) {
+   switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "scissors") {
+                humanScore++;
+                return "You win! Rock beats Scissors.";
+            } else if (computerChoice === "paper") {
+                computerScore++;
+                return "You lose! Paper beats Rock.";
+            } else {
+                return "It's a tie!";
+            }
+        case "paper":
+            if (computerChoice === "rock") {
+                humanScore++;
+                return "You win! Paper beats Rock.";
+            } else if (computerChoice === "scissors") {
+                computerScore++;
+                return "You lose! Scissors beats Paper.";
+            } else {
+                return "It's a tie!";
+            }
+        case "scissors":
+            if (computerChoice === "paper") {
+                humanScore++;
+                return "You win! Scissors beats Paper.";
+            } else if (computerChoice === "rock") {
+                computerScore++;
+                return "You lose! Rock beats Scissors.";
+            } else {
+                return "It's a tie!";
+            }
+    }
+}
+
+let getHumanSelection = getHumanChoice();
+let getComputerSelection = getComputerChoice();
+console.log(playRound(getHumanSelection, getComputerSelection));
+console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
