c1d96b4bef5802e0e80d01f423c6848e50a919b8
DiosTlaloc <diostlaloc5@gmail.com>
2026-01-02 14:57:05 -0800

    Implemented getComputerChoice and getHumanChoice functions

--- mainscript.js
+++ mainscript.js
@@ -1,4 +1,12 @@
 
-function greet() {
-    console.log("Hello, World!");
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let secondPlayerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return secondPlayerChoice;
+}
+
+function getHumanChoice() {
+    const humanChoice = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
+    return humanChoice
 }
